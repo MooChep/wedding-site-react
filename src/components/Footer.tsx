385439f6422de2385d9ba8c3593865677cdff736
MooChep <ilan.heas@supdevinci-edu.fr>
2026-02-20 20:12:58 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,38 @@
+import CustomHrLine from "@/components/CustomHrLine";
+
 export default function Footer() {
   return (
-    <footer className="text-white mt-20">
-      <div className="flex flex-col items-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row justify-center w-full py-10">
+    <footer className="bg-gray-100 mt-10">
+
+      <CustomHrLine/>
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row justify-around w-full py-10 text-center">
+          
           <div>
-            <h3 className="text-xl font-bold mb-4 var(--color-pink)">Informations & Contact</h3>
-            <p className="text-gray-400">Domaine Les 3 Rivages</p>
+            <h3 className="text-xl font-bold mb-6" style={{ color: 'var(--color-blue)' }}>
+              Informations & Contacts
+            </h3>
+            <p className="text-gray-700 mb-3">Domaine les 3 Rivages</p>
             <a
               href="https://www.google.com/maps/dir/?api=1&destination=Domaine+les+3+Rivages"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-rose-400 hover:underline mt-2 inline-block"
+              className="text-gray-700 hover:text-rose-400 transition mb-3 inline-block"
             >
-              Voir l’itinéraire sur Google Maps
+              Voir l'itinéraire sur GoogleMaps
             </a>
-
-            <a href="mailto:camille.ilan1909@gmail.com" className="text-rose-400 hover:underline mt-2 inline-block">
-              Envoyer un email
+            <br/>
+            <a href="mailto:camille.ilan1909@gmail.com" className="text-gray-700 hover:text-rose-400 transition mt-3 inline-block">
+              Camille.ilan1909@gmail.com
             </a>
           </div>
 
           <div>
-            <h4 className="text-xl font-bold mb-4 text-pink-500">Actions rapides</h4>
-            <ul className="space-y-2 text-gray-400">
+            <h4 className="font-schoolbell text-xl font-bold mb-6" style={{ color: 'var(--color-pink)' }}>
+              Actions rapides
+            </h4>
+            <ul className="space-y-3 text-gray-700">
               <li><a href="/" className="hover:text-rose-400 transition">Retour à l'accueil</a></li>
               <li><a href="/faq" className="hover:text-rose-400 transition">Poser une question</a></li>
               <li><a href="/rsvp" className="hover:text-rose-400 transition">Confirmer sa présence</a></li>
@@ -30,10 +40,6 @@ export default function Footer() {
             </ul>
           </div>
 
-        </div>
-
-        <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2026 FLX. Tous droits reservés.</p>
         </div>
       </div>
     </footer>
